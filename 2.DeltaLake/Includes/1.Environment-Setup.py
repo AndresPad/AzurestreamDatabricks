e451904c9c716ef38cf686ed429ee378707e0c81
Andres Padilla <69865428+AndresPad@users.noreply.github.com>
2022-05-31 17:22:48 -0400

    3rd

---
+++ 2.DeltaLake/Includes/1.Environment-Setup.py
@@ -0,0 +1,16 @@
+# Databricks notebook source
+# MAGIC %python
+# MAGIC course_name = "Azurestream Databricks Delta Lake Enviornment Setup"
+
+# COMMAND ----------
+
+# MAGIC %run "./2.Dataset-Mounts"
+
+# COMMAND ----------
+
+# MAGIC %run "./3.Create-User-DB"
+
+# COMMAND ----------
+
+# MAGIC %python
+# MAGIC displayHTML("<p>Azurestream Databricks Delta Lake Enviornment Setup - All done!</p>")
